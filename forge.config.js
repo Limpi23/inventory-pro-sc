23d52ea28ca03b697a95dc85019ee2a569a3950f
Jonathan <jonathan.barrero.a@gmail.com>
2025-05-08 21:58:57 -0400

    fix forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -1,6 +1,3 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
-
 module.exports = {
   packagerConfig: {
     asar: true,
@@ -16,18 +13,6 @@ module.exports = {
         manufacturer: 'SuitCore',
       },
     },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
   ],
   plugins: [
     {
@@ -36,14 +21,14 @@ module.exports = {
     },
     // Fuses are used to enable/disable various Electron functionality
     // at package time, before code signing the application
-    new FusesPlugin({
-      version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: false,
-      [FuseV1Options.EnableCookieEncryption]: true,
-      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-      [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    new (require('@electron-forge/plugin-fuses').FusesPlugin)({
+      version: require('@electron/fuses').FuseVersion.V1,
+      [require('@electron/fuses').FuseV1Options.RunAsNode]: false,
+      [require('@electron/fuses').FuseV1Options.EnableCookieEncryption]: true,
+      [require('@electron/fuses').FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [require('@electron/fuses').FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [require('@electron/fuses').FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [require('@electron/fuses').FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
 };
