df2b80691edabcad4761bcee4682193516af3369
Jonathan <jonathan.barrero.a@gmail.com>
2025-05-08 22:14:45 -0400

    added new configuration forge.config.cjs

---
+++ forge.config.cjs
@@ -0,0 +1,37 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    icon: 'src/assets/app-icon',
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-wix',
+      config: {
+        language: 3082,
+        description: 'Inventario Pro - SC',
+        manufacturer: 'SuitCore',
+        exe: 'inventory-suit.exe',
+        name: 'inventory-suit'
+      },
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};
